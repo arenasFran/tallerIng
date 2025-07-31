cf7d5ca3dd400442fbc04210a10301132726aacd
MatiOreiro <mo239479@fi365.ort.edu.uy>
2025-07-31 11:51:19 -0300

    test impeca

--- app.test.js
+++ app.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { loadSelects, Barbero, Servicio } from './app.js';
 
 test('loadSelects carga correctamente barberos y servicios', () => {
@@ -7,25 +6,14 @@ test('loadSelects carga correctamente barberos y servicios', () => {
     <select id="barberoSelect"></select>
   `;
 
-  loadSelects(
-    [new Barbero('Juan', 'Pérez', 'Fades')],
-    [new Servicio('Corte', 500)]
-  );
+  loadSelects();
 
   const servicioSelect = document.getElementById('servicioSelect');
   const barberoSelect = document.getElementById('barberoSelect');
 
-  expect(servicioSelect.children.length).toBe(2); // placeholder + 1 opción
+  expect(servicioSelect.children.length).toBe(6); 
   expect(servicioSelect.children[1].textContent).toBe('Corte ($500)');
 
-  expect(barberoSelect.children.length).toBe(2);
-  expect(barberoSelect.children[1].textContent).toBe('Juan Pérez - Fades');
+  expect(barberoSelect.children.length).toBe(7);
+  expect(barberoSelect.children[1].textContent).toBe('Joaquín Rojas - Especialista en fades');
 });
-=======
-
-
-test('Cargar selects de barberos en reserva', function () {
-    const resultado  = loadSelects();
-    expect(resultado).toEqual('<option value="" disabled="">Selecciona un barbero</option><option value="Joaquín Rojas">Joaquín Rojas - Especialista en fades</option><option value="Matías Suárez">Matías Suárez - Maestro de navaja</option><option value="Ignacio Pereira">Ignacio Pereira - Artista de barbas</option><option value="Sebastián Delgado">Sebastián Delgado - Colorista profesional</option><option value="Lucas Méndez">Lucas Méndez - Terapeuta capilar</option><option value="Diego Montes">Diego Montes - Estilista vintage</option>');
-})
->>>>>>> 1eb51e50d13f68a0b69738af70391258e6d1bfcb
